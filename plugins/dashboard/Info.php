83775e51f86de069f648aefbf162894d478c9b2c
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-06-21 22:44:38 +0800

    first app

---
+++ plugins/dashboard/Info.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name'          =>  'Dashboard',
+    'description'   =>  'Akses cepat pada modul dan berita.',
+    'author'        =>  'Basoro.ID',
+    'version'       =>  '1.1',
+    'compatibility' =>  '2022',
+    'icon'          =>  'home',
+    'pages'         =>  ['Main' => 'main'] 
+];
---
+++ plugins/profil/Info.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'name'          =>  'Profil',
+    'description'   =>  'Modul profil',
+    'author'        =>  'Basoro.ID',
+    'version'       =>  '1.1',
+    'compatibility' =>  '2022',
+    'icon'          =>  'address-book'
+];
